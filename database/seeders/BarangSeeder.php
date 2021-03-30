2850fbd3ce587058ac50554b5a2a0e4d03497d9a
Genadi Dharma <genadislawa@gmail.com>
2021-03-30 09:10:26 +0800

    barang seeder filled

--- database/seeders/BarangSeeder.php
+++ database/seeders/BarangSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use Illuminate\Support\Facades\DB;
 
 class BarangSeeder extends Seeder
 {
@@ -13,6 +16,24 @@ class BarangSeeder extends Seeder
      */
     public function run()
     {
-        //
+
+        $faker = Faker::create('id_ID');
+
+        for ($i = 0; $i < 20; $i++) {
+            $kategori = ['Makanan', 'Minuman', 'Snack', 'Rempah-rempah'];
+            $kategori_rand = array_rand($kategori, 1);
+            
+            DB::table('barang')->insert([
+                [
+                    'kode_barang' => 'PRD' . $faker->randomNumber(3),
+                    'nama_barang' => $faker->word(),
+                    'kategori_barang' => $kategori[$kategori_rand],
+                    'harga' => round($faker->numberBetween(5000, 30000), -3),
+                    'qty' => $faker->numberBetween(1, 100),
+                    'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                    'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+                ]
+            ]);
+        }
     }
 }
